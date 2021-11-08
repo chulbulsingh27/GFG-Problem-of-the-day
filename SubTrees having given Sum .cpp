cda5102743c28f74017328de6720576c748c1b4a
chulbulsingh27 <66539055+chulbulsingh27@users.noreply.github.com>
2021-11-08 09:07:15 +0530

    Update SubTrees having given Sum .cpp

--- SubTrees having given Sum .cpp
+++ SubTrees having given Sum .cpp
@@ -13,3 +13,35 @@ int countSubtreesWithSumX(Node* root, int x)
     return count;
 	// Code here
 }
+
+
+
+//Given a binary tree and an integer X. Your task is to complete the function countSubtreesWithSumX() that returns the count of the number of subtress having total node’s data sum equal to the value X.
+//Example: For the tree given below:            
+
+              5
+            /    \
+        -10     3
+        /    \    /  \
+      9     8  -4 7
+
+Subtree with sum 7:
+             -10
+            /      \
+          9        8
+
+and one node 7.
+
+Example 1:
+
+Input:
+       5
+    /    \
+  -10     3
+ /   \   /  \
+ 9   8 -4    7
+X = 7
+Output: 2
+Explanation: Subtrees with sum 7 are
+[9, 8, -10] and [7] (refer the example
+in the problem description).

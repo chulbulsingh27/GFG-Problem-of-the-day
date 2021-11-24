a41353bd2da57928b16ce6cda15388f68eab247d
chulbulsingh27 <66539055+chulbulsingh27@users.noreply.github.com>
2021-11-24 17:15:51 +0530

    Create Trapping Rain Water .cpp

---
+++ Trapping Rain Water .cpp
@@ -0,0 +1,27 @@
+ Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, 
+ compute how much water can be trapped between the blocks during the rainy season. 
+ Example 1:
+Input:
+N = 6
+arr[] = {3,0,0,2,0,4}
+Output:
+10
+
+  //code
+  
+  class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        int left[n],right[n];
+        left[0] = arr[0];
+        for(int i=1;i<n;i++) left[i] = max(left[i-1],arr[i]);
+        right[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--) right[i] = max(right[i+1],arr[i]);
+        long long ans = 0;
+        for(int i=0;i<n;i++) ans += min(left[i],right[i]) - arr[i];
+        return ans;
+
+    }
+};

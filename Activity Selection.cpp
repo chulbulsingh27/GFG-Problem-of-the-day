eba2791fe4863d5eb325cf726e07a18f71ce90ee
chulbulsingh27 <66539055+chulbulsingh27@users.noreply.github.com>
2021-11-27 10:11:10 +0530

    Update Activity Selection.cpp

--- Activity Selection.cpp
+++ Activity Selection.cpp
@@ -1,4 +1,6 @@
-Given N activities with their start and finish day given in array start[ ] and end[ ]. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a given day.
+Given N activities with their start and finish day given in array start[ ] and end[ ]. 
+Select the maximum number of activities that can be performed by a single person,
+assuming that a person can only work on a single activity at a given day.
 Note : Duration of the activity includes both starting and ending day.
 
 

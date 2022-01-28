1058a4ad280cb8cf9efe64d03fed6ab94123fe81
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-01-28 22:32:53 +0530

    Create Handshakes .cpp

---
+++ Handshakes .cpp
@@ -0,0 +1,43 @@
+We have N persons sitting on a round table. Any person can do a handshake with any other person.
+
+     1
+2         3
+     4
+
+Handshake with 2-3 and 1-4 will cause cross.
+
+In how many ways these N people can make handshakes so that no two handshakes cross each other. N would be even. 
+ 
+
+Example 1:
+
+Input:
+N = 2
+Output:
+1
+Explanation:
+{1,2} handshake is
+possible.
+  
+  // code
+  
+  We have N persons sitting on a round table. Any person can do a handshake with any other person.
+
+     1
+2         3
+     4
+
+Handshake with 2-3 and 1-4 will cause cross.
+
+In how many ways these N people can make handshakes so that no two handshakes cross each other. N would be even. 
+ 
+
+Example 1:
+
+Input:
+N = 2
+Output:
+1
+Explanation:
+{1,2} handshake is
+possible.

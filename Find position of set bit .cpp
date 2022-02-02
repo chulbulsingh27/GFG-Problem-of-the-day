d3a009ef2ef0944524f3fc04aa62ecb9e0fc08c7
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-02-02 09:43:39 +0530

    Create Find position of set bit .cpp

---
+++ Find position of set bit .cpp
@@ -0,0 +1,24 @@
+Given a number N having only one ‘1’ and all other ’0’s in its binary representation, find position of the only set bit. 
+If there are 0 or more than 1 set bit the answer should be -1. Position of  set bit '1' should be counted starting with 1 from LSB side in binary representation of the number.
+
+Example 1:
+
+Input:
+N = 2
+Output:
+2
+Explanation:
+2 is represented as "10" in Binary.
+As we see there's only one set bit
+and it's in Position 2 and thus the
+Output 2.
+  
+  // code
+  
+  class Solution {
+  public:
+    int findPosition(int n) {
+        if(n == 0 || (n&(n-1)) !=0) return -1;
+        return log2(n)+1;
+    }
+};
